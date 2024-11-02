697682e35b039a8b6c0b3c3db37baa1f62442ea5
jhuai <huai.3@osu.edu>
2024-11-02 17:13:35 +0800

    get snapshots from mid360 rosbags

--- get_snapshots_from_bag.py
+++ get_snapshots_from_bag.py
@@ -13,17 +13,16 @@ import struct
 import sensor_msgs.point_cloud2 as pc2
 
 
-hesai_fieldnames = ['x', 'y', 'z', 'intensity', 'timestamp', 'ring']
-hesai_fieldtypecodes = ('f', 'f', 'f', 'f', 'd', 'H')
-
-def write_pcd_file(msg, fieldnames, filename):
+def write_hesai_pcd_file(msg, filename):
     """Write a PointCloud2 message to a PCD file.
     https://pointclouds.org/documentation/tutorials/pcd_file_format.html
     """
+    hesai_fieldnames = ['x', 'y', 'z', 'intensity', 'timestamp', 'ring']
+    hesai_fieldtypecodes = ('f', 'f', 'f', 'f', 'd', 'H') # https://docs.python.org/3/library/struct.html
     with open(filename, 'wb') as f:
         f.write(bytearray("# .PCD v0.7 - Point Cloud Data file format\n", 'utf-8'))
         f.write(bytearray("VERSION 0.7\n", 'utf-8'))
-        f.write(bytearray(f"FIELDS {' '.join(fieldnames)}\n", 'utf-8'))
+        f.write(bytearray(f"FIELDS {' '.join(hesai_fieldnames)}\n", 'utf-8'))
         f.write(bytearray("SIZE 4 4 4 4 8 2\n", 'utf-8'))
         f.write(bytearray("TYPE F F F F F U\n", 'utf-8'))
         f.write(bytearray("COUNT 1 1 1 1 1 1\n", 'utf-8'))
@@ -32,9 +31,33 @@ def write_pcd_file(msg, fieldnames, filename):
         f.write(bytearray("VIEWPOINT 0 0 0 1 0 0 0\n", 'utf-8'))
         f.write(bytearray(f"POINTS {msg.width}\n", 'utf-8'))
         f.write(bytearray("DATA binary\n", 'utf-8'))
-        for point in pc2.read_points(msg, field_names=fieldnames):
+        for point in pc2.read_points(msg, field_names=hesai_fieldnames):
             for i, p in enumerate(point):
                 f.write(struct.pack(hesai_fieldtypecodes[i], p))
+
+
+def write_livox_ros_pcd_file(msg, filename):
+    livox_ros_fields = ['x', 'y', 'z', 'intensity', 'tag', 'line', 'timestamp']
+    livox_ros_fieldcodes = ['f', 'f', 'f', 'f', 'B', 'B', 'd']
+    
+    with open(filename, 'wb') as f:
+        # Write the PCD header
+        f.write(bytearray("# .PCD v0.7 - Point Cloud Data file format\n", 'utf-8'))
+        f.write(bytearray("VERSION 0.7\n", 'utf-8'))
+        f.write(bytearray(f"FIELDS {' '.join(livox_ros_fields)}\n", 'utf-8'))
+        f.write(bytearray("SIZE 4 4 4 4 1 1 8\n", 'utf-8'))
+        f.write(bytearray("TYPE F F F F U U F\n", 'utf-8'))
+        f.write(bytearray("COUNT 1 1 1 1 1 1 1\n", 'utf-8'))
+        f.write(bytearray(f"WIDTH {msg.width}\n", 'utf-8'))
+        f.write(bytearray("HEIGHT 1\n", 'utf-8'))
+        f.write(bytearray("VIEWPOINT 0 0 0 1 0 0 0\n", 'utf-8'))
+        f.write(bytearray(f"POINTS {msg.width}\n", 'utf-8'))
+        f.write(bytearray("DATA binary\n", 'utf-8'))
+        
+        # Write the binary data for each point
+        for point in pc2.read_points(msg, field_names=livox_ros_fields):
+            for i, p in enumerate(point):
+                f.write(struct.pack(livox_ros_fieldcodes[i], p))
 
 
 def get_snapshots_from_bag(bagfile, outdir, image_topic, lidar_topic):
@@ -109,10 +132,16 @@ def get_snapshots_from_bag(bagfile, outdir, image_topic, lidar_topic):
         elif topic == lidar_topic:
             if abs(t - front_times[1]) < rospy.Duration(0.01):
                 lidar_path = os.path.join(front_dir, f'{t.secs}.{t.nsecs:09d}.pcd')
-                write_pcd_file(msg, hesai_fieldnames, lidar_path)
+                if 'hesai' in topic:
+                    write_hesai_pcd_file(msg, lidar_path)
+                elif 'livox' in topic:
+                    write_livox_ros_pcd_file(msg, lidar_path)
             elif abs(t - back_times[1]) < rospy.Duration(0.01):
                 lidar_path = os.path.join(back_dir, f'{t.secs}.{t.nsecs:09d}.pcd')
-                write_pcd_file(msg, hesai_fieldnames, lidar_path)
+                if 'hesai' in topic:
+                    write_hesai_pcd_file(msg, lidar_path)
+                elif 'livox' in topic:
+                    write_livox_ros_pcd_file(msg, lidar_path)
     bag.close()
 
 
