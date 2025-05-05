cdaa1dc9bac75c443d8aa40012f1f0543e7cfa79
jhuai <huai.3@osu.edu>
2025-05-05 15:18:15 +0800

    remove deprecated files

--- reverse_bag/reverse_bags_batch.sh
+++ reverse_bag/reverse_bags_batch.sh
@@ -3,9 +3,9 @@
 
 script=/home/$USER/Documents/lidar/whu_tls_regis/reverse_bag/reverse_bag.py
 
-python3 $script /media/$USER/BackupPlus/jhuai/results/ref_trajs_all \
+python3 -u $script /media/$USER/BackupPlus/jhuai/results/ref_trajs_all \
     /media/$USER/My_Book/jhuai/data/zip /media/$USER/BackupPlus/jhuai/results/front_back_snapshots 2>&1 | tee /media/$USER/BackupPlus/jhuai/results/front_back_snapshots/reverse.log
 
-python3 $script /media/$USER/MyBookDuo/jhuai/results/ref_trajs_all \
+python3 -u $script /media/$USER/MyBookDuo/jhuai/results/ref_trajs_all \
     /media/$USER/MyBookDuo/jhuai/data/zip /media/$USER/MyBookDuo/jhuai/results/front_back_snapshots 2>&1 | tee /media/$USER/MyBookDuo/jhuai/results/front_back_snapshots/reverse.log
 
